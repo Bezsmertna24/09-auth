1ab50c0882793affd524fee54467c6423b9bd1dd
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-09-21 11:26:28 +0300

    correct

--- app/(private routes)/notes/action/create/page.tsx
+++ app/(private routes)/notes/action/create/page.tsx
@@ -17,7 +17,7 @@ export default function CreateNote() {
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
-        <NoteForm />
+        <NoteForm onClose={() => {}}/>
       </div>
     </main>
   );
